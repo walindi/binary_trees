5c246e4b56af9eadbe96e3f311827214b27c59af
walindi <walindibarack@yahoo.com>
2023-06-11 00:28:27 +0300

    Add prototype

--- binary_trees.h
+++ binary_trees.h
@@ -50,6 +50,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+bst_t *bst_search(const bst_t *tree, int value);
 
 
 #endif
